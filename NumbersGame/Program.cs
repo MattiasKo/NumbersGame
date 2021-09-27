02ed1c98437c257ca6f3ab79109338a79ec90163
Gos <Gos@DESKTOP-NP9Q5EE>
2021-09-27 15:55:11 +0200

    Random range +1 för att den ska ta med hela talet.

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -15,7 +15,9 @@ namespace NumbersGame
             Console.WriteLine("Welcome! can you guess what number i'm thinking off? You got 5 tries, Select difficulty" +
                 "\n1: Easy, number between 1-10" +
                 "\n2: Normal, number between 1-20" +
-                "\n3: Hard, number between 1 - 50");
+                "\n3: Hard, number between 1 - 50"+
+                "\n4: Very hard, number between 1 - 100");
+
             byte Difficulty = byte.Parse(Console.ReadLine());
             int DiffNum = 10;
             switch (Difficulty)
@@ -32,6 +34,10 @@ namespace NumbersGame
                     DiffNum = 50;
                     break;
 
+                case 4:
+                    DiffNum = 100;
+                    break;
+
                 default:
                     DiffNum = 10;
                     break;
@@ -40,26 +46,12 @@ namespace NumbersGame
             byte GLeft = 5;
             Random random = new Random();
 
-            int RightNum = random.Next(1, DiffNum);               
+            int RightNum = random.Next(1, DiffNum+1);               
 
                 GuessN(RightNum, GLeft, DiffNum);
 
         }
-        public static void EndOfGame()
-        {
-            Console.WriteLine("Try agian? (Y/N)");
-            string YorN = Console.ReadLine();
-            if(YorN == "Y"||YorN == "y")
-            {
-                Program.StartGame();
-            }
-            else
-            {
-                Console.WriteLine("Thanks for playing, bye!");
-            }
-            
-            
-        }
+      
         public static void GuessN(int RightNum, byte GLeft, int Diffnum)
         {
             do
@@ -86,26 +78,42 @@ namespace NumbersGame
                 {
                     Console.Write("To high.");
                 }
-                if (GuessNum - RightNum >= -CloseNum && GuessNum - RightNum <= CloseNum && RightNum != GuessNum) // om numret är 10% ifrån rätt svar och samtidigt felsvar
+                if (GuessNum - RightNum >= -CloseNum && GuessNum - RightNum <= CloseNum && RightNum != GuessNum) // om numret är 10% ifrån rätt svar och samtidigt felsvar säg får man ett extra meddelande "close!"
                 {
                     Console.Write(" close!");
                 }
-                //if (GuessNum - RightNum < -FarOff && GuessNum - RightNum > FarOff && RightNum != GuessNum) // om numret är 10% ifrån rätt svar och samtidigt felsvar
-                //{
-                //    Console.Write(" To far away!");
-                //}
+              
 
                 if (GLeft == 0 && GuessNum != RightNum)
                 {
                     Console.WriteLine(" You lost!");
                     Program.EndOfGame();
                 }
-                if (0 < GLeft && GuessNum != RightNum)
+                if (1 < GLeft && GuessNum != RightNum)
                 {
                     Console.WriteLine(" You have " + GLeft + " guesses left. Guess agian!");
                 }
+                if (1 == GLeft && GuessNum != RightNum)
+                {
+                    Console.WriteLine(" You have " + GLeft + " guess left. Guess agian!");
+                }
 
             } while (0 < GLeft);
+
+        }
+        public static void EndOfGame()
+        {
+            Console.WriteLine("Try agian? (Y/N)");
+            string YorN = Console.ReadLine();
+            if (YorN == "Y" || YorN == "y")
+            {
+                Program.StartGame();
+            }
+            else
+            {
+                Console.WriteLine("Thanks for playing, bye!");
+            }
+
 
         }
     }
